76e003394bf00ba91d69de4a100da6f5d14929f0
[pkjinfinity] <[pkjinfinity@outlook.com]>
2025-02-28 10:25:15 +0800

    windows ci bug fix ref 2

--- conanfile.py
+++ conanfile.py
@@ -19,13 +19,14 @@ class ConanApplication(ConanFile):
         # requirements = self.conan_data.get('requirements', [])
         # for requirement in requirements:
         #     self.requires(requirement)
-        self.requires("catch2/3.7.1")
-        self.requires("spdlog/1.14.1")
-        self.requires("jsoncpp/1.9.6")
-        self.requires("dbg-macro/0.5.1")
-        self.requires("opencv/4.10.0")
-        self.requires("boost/1.86.0")
-        self.requires("cli11/2.4.2")
+        # self.requires("catch2/3.7.1")
+        # self.requires("spdlog/1.14.1")
+        # self.requires("jsoncpp/1.9.6")
+        # self.requires("dbg-macro/0.5.1")
+        # self.requires("opencv/4.10.0")
+        # self.requires("boost/1.86.0")
+        # self.requires("cli11/2.4.2")
+        self.requires("libx264/cci.20240224")
     def configure(self):
         self.options['opencv'].with_wayland = False
         self.options['opencv'].shared = True
